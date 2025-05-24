613f03b0be753f082ba21ec4efca8fd17c7e5aba
Judahh <judahholanda7@gmail.com>
2025-05-23 22:03:19 -0300

    fix

--- api/ponto.ts
+++ api/ponto.ts
@@ -10,80 +10,84 @@ import { chromium } from 'playwright-core';
 function getPassword(username: string): string {
     const password = process.env[username] || process.env[username.replaceAll('.', '_')];
     return password || '';
-    }
+}
 
 export async function registrarPonto(username: string, url: string, latitude?: string, longitude?: string) {
-  const agora = new Date().toISOString();
-  try {
-    // const browser =  await playwright.launchChromium();
-    
-    const executablePath = await chromiumBinary.executablePath();
+    const agora = new Date().toISOString();
+    try {
+        // const browser =  await playwright.launchChromium();
+
+        const executablePath = await chromiumBinary.executablePath();
 
         // launch browser with external Chromium
-    const browser = await chromium.launch({
-        args: chromiumBinary.args,
-        executablePath: executablePath,
-        headless: true,
-    });
+        const browser = await chromium.launch({
+            args: chromiumBinary.args,
+            executablePath: executablePath,
+            headless: true,
+        });
 
-    const context = await browser.newContext();
-    const page = (await context.newPage()) as any;
+        const context = await browser.newContext();
+        const page = (await context.newPage()) as any;
 
-    await page.route('**/*', (route: any) => {
-        if (
-            ['image', 'stylesheet', 'font'].includes(
-                route.request().resourceType()
-            )
-        ) {
-            route.abort();
-        } else {
-            route.continue();
-        }
-    });
-
-    await page.evaluate((o?: {lat?: string | number, lon?: string | number}) => {
-        // @ts-ignore
-        (navigator as any).geolocation.getCurrentPosition = function (success: any) {
-          success({
-            coords: {
-              latitude: o?.lat,
-              longitude: o?.lon,
-              accuracy: 100
+        await page.route('**/*', (route: any) => {
+            if (
+                ['image', 'stylesheet', 'font'].includes(
+                    route.request().resourceType()
+                )
+            ) {
+                route.abort();
+            } else {
+                route.continue();
             }
-          });
+        });
+
+        await page.evaluate((o?: { lat?: string | number, lon?: string | number }) => {
+            // @ts-ignore
+            (navigator as any).geolocation.getCurrentPosition = function (success: any) {
+                success({
+                    coords: {
+                        latitude: o?.lat,
+                        longitude: o?.lon,
+                        accuracy: 100
+                    }
+                });
+            };
+        }, { latitude, longitude });
+
+        await page.goto(url);
+
+        await page.waitForSelector('#usuario');
+        await page.waitForSelector('#senha');
+
+        await page.fill('#usuario', username);
+        await page.fill('#senha', getPassword(username)); // TODO: Obter dinamicamente
+
+        await page.keyboard.press('Enter');
+
+        await page.waitForSelector('#btnRegistrar');
+        await page.click('#btnRegistrar');
+
+        console.log('div.alert.alert-success.growl-animated');
+
+        const confirmation = await page.waitForSelector('div.alert.alert-success.growl-animated', {
+            state: 'visible',
+            timeout: 20000, // 20 seconds
+        });
+        await browser.close();
+
+        return {
+            usuario: username,
+            hora: agora,
+            mensagem: `Registrando ponto às ${agora}`,
+            confirmacao: confirmation
         };
-      }, {latitude, longitude});
 
-    await page.goto(url);
-
-    await page.waitForSelector('#usuario');
-    await page.waitForSelector('#senha');
-
-    await page.fill('#usuario', username);
-    await page.fill('#senha', getPassword(username)); // TODO: Obter dinamicamente
-
-    await page.keyboard.press('Enter');
-
-    await page.waitForSelector('#btnRegistrar');
-    await page.click('#btnRegistrar');
-
-    const confirmation = await page.textContent('div.alert.alert-success.growl-animated');
-
-    await browser.close();
-
-    return {
-      usuario: username,
-      hora: agora,
-      mensagem: `Registrando ponto às ${agora}`,
-      confirmacao: confirmation
-    };
-
-  } catch (error: any) {
-    return {
-      usuario: username,
-      hora: agora,
-      mensagem: `Erro ao registrar ponto: ${error.message}`,
-      error: error.stack
-    };
-  }
+    } catch (error: any) {
+        return {
+            usuario: username,
+            hora: agora,
+            mensagem: `Erro ao registrar ponto: ${error.message}`,
+            error: error.stack
+        };
+    }
 }
